783e343393a51e104e7252f998fdd1228f2858b8
Honlan <honlan@192.168.0.103>
2016-03-23 19:08:13 +0800

    add admin pages

---
+++ Application/Home/Controller/AdminController.class.php
@@ -0,0 +1,55 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class AdminController extends Controller{
+	public function _initialize(){
+		if(!isset($_SESSION['urole']) || $_SESSION['urole'] < 2){
+			$this->redirect('Index/contest', array('id'=>1));
+		}
+	}
+
+	public function index(){
+		$this->display();
+	}
+
+	public function contest_pay_confirm(){
+		$submissions = M('submission')->where(array('contest_id'=>1, 'ispaied'=>0, 'pay_confirm'=>1))->select();
+		$tmp = array();
+		foreach ($submissions as $key => $value) {
+			$user = M('user')->where(array('id'=>$value['user_id']))->find();
+			$value['user_email'] = $user['email'];
+			$value['user_companyc'] = $user['companyc'];
+			$tmp[] = $value;
+		}
+		$this->submissions = $tmp;
+		$this->display();
+	}
+
+	public function contest_pay_yes(){
+		M('submission')->where(array('id'=>I('id')))->save(array('ispaied'=>1, 'pay_confirm'=>0));
+		$email = M('email')->where(array('name'=>'确认支付'))->find();
+		$email_content = $email['content'];
+		if(count(explode("\n", $email_content)) == 1 ){
+			$email_content = explode("\r", $email_content);
+		} else {
+			$email_content = explode("\n", $email_content);
+		}
+		$temp = '';
+		foreach ($email_content as $key => $value) {
+			$temp .= $value."<br/>";
+		}
+		$email_content = $temp;
+		$email_content = explode("^^^", $email_content);
+		$email_content = $email_content[0].U('Contest/info', array('id'=>I('id')),false,true).$email_content[1];
+		$user_email = M('user')->where(array('id'=>I('user_id')))->find();
+		$user_email = $user_email['email'];
+		SendMail($user_email, $email['title'], $email_content);
+		$this->redirect('Admin/contest_pay_confirm');
+	}
+
+	public function contest_pay_no(){
+		M('submission')->where(array('id'=>I('id')))->save(array('ispaied'=>0, 'pay_confirm'=>0));
+		$this->redirect('Admin/contest_pay_confirm');
+	}
+}
+?>
