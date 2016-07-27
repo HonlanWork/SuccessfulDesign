de719700813c854c2b95ba5e6ef963e7763abfe2
Honlan <honlan@HonlandeMacBook-Air.local>
2016-07-27 17:57:22 +0800

    add api

--- Application/Home/Controller/ContestController.class.php
+++ Application/Home/Controller/ContestController.class.php
@@ -632,6 +632,31 @@ class ContestController extends CommonController {
 		$this->redirect('User/submissions');
 	}
 
+	public function promotion_pay_success_api(){
+		M('promotion')->where(array('submission_id'=>I('submission_id'),'promotion_code'=>I('promotion_code')))->save(array('ispaied'=>1,'promotion_code'=>''));
+		$email = M('email')->where(array('name'=>'购买推广'))->find();
+    	$email_content = $email['content'];
+    	if(count(explode("\n", $email_content)) == 1 ){
+    		$email_content = explode("\r", $email_content);
+    	} else {
+    		$email_content = explode("\n", $email_content);
+    	}
+    	$temp = '';
+    	foreach ($email_content as $key => $value) {
+    		$temp .= $value."<br/>";
+    	}
+    	$email_content = $temp;
+    	$submission = M('submission')->where(array('id'=>I('submission_id'), 'user_id'=>$_SESSION['uid']))->find();
+    	$tmp = '作品中文名称：'.$submission['titlec'].'<br/>作品英文名称：'.$submission['titlee'].'<br/>作品类别：'.$submission['category'];
+        $email_content = explode("^^^", $email_content);
+    	$email_content = $email_content[0].$tmp.$email_content[1];
+    	$admins = M('user')->where(array('role'=>2))->select();
+    	foreach ($admins as $key => $value) {
+    		SendMail($value['email'], $email['title'], $email_content);
+    	}
+    	echo json_encode(array('submission_id'=>I('submission_id'), 'promotion_code'=>I('promotion_code')));
+	}
+
 	public function promotion_pay_cancel(){
 		// M('promotion')->where(array('submission_id'=>I('submission_id'),'promotion_code'=>I('promotion_code')))->delete();
 		$this->redirect('Contest/promotion');
